f9d95ce1bd78eb401a1072e7b12d10be6aaa3864
Afsinur <afsin.sayem1@gmail.com>
2023-01-05 18:48:18 +0600

    token click drop down added

--- js/app.js
+++ js/app.js
@@ -16,4 +16,24 @@ qs_a("*").forEach((itm, i) => {
       add(qs_a(".showProducts")[0], "dec-opcityAnim");
     }
   });
+
+  on(itm, "click", (e) => {
+    let tkn = e.target.dataset.tkn;
+
+    if (tkn === undefined || tkn === null) {
+      remove(qs_a(".TokenDropDownContainer")[0], "opcityAnim");
+      remove(qs_a(".TokenDropDownContainer")[0], "dec-opcityAnim");
+
+      add(qs_a(".TokenDropDownContainer")[0], "dec-opcityAnim");
+    }
+  });
+});
+
+//-------------------------------
+
+on(qs_a(".dropToken")[0], "click", () => {
+  remove(qs_a(".TokenDropDownContainer")[0], "opcityAnim");
+  remove(qs_a(".TokenDropDownContainer")[0], "dec-opcityAnim");
+
+  add(qs_a(".TokenDropDownContainer")[0], "opcityAnim");
 });

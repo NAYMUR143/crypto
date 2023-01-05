76c28aae60c1b261644162a4dd296af7ac0e1bfe
Afsinur <afsin.sayem1@gmail.com>
2023-01-05 19:14:11 +0600

    token dropdown-updated

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
+let tamp_arr = [
+  `
+  <img src="./img/CRO.webp" alt="" data-tkn />
+  <span data-tkn>CRO</span>
+  `,
+  `
+  <img src="./img/btc.webp" alt="" data-tkn />
+  <span data-tkn>BTC</span>
+  `,
+  `
+  <img src="./img/eth.webp" alt="" data-tkn />
+  <span data-tkn>ETH</span>
+  `,
+];
+
 on(qs_a(".mouseOver")[0], "mouseover", () => {
   remove(qs_a(".showProducts")[0], "opcityAnim");
   remove(qs_a(".showProducts")[0], "dec-opcityAnim");
@@ -21,10 +36,7 @@ qs_a("*").forEach((itm, i) => {
     let tkn = e.target.dataset.tkn;
 
     if (tkn === undefined || tkn === null) {
-      remove(qs_a(".TokenDropDownContainer")[0], "opcityAnim");
-      remove(qs_a(".TokenDropDownContainer")[0], "dec-opcityAnim");
-
-      add(qs_a(".TokenDropDownContainer")[0], "dec-opcityAnim");
+      hideTokens();
     }
   });
 });
@@ -37,3 +49,19 @@ on(qs_a(".dropToken")[0], "click", () => {
 
   add(qs_a(".TokenDropDownContainer")[0], "opcityAnim");
 });
+
+qs_a(".token-item").forEach((itm, i) => {
+  on(itm, "click", (e) => {
+    qs_a(".dyn-DATA")[0].innerHTML = tamp_arr[i];
+    hideTokens();
+  });
+});
+
+//---------------------------------------
+
+function hideTokens() {
+  remove(qs_a(".TokenDropDownContainer")[0], "opcityAnim");
+  remove(qs_a(".TokenDropDownContainer")[0], "dec-opcityAnim");
+
+  add(qs_a(".TokenDropDownContainer")[0], "dec-opcityAnim");
+}
